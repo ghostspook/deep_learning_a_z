ffbcc70ffd443584fd134f339be2d57ebcc01a2a
Rafael Castillo <ghostspook@users.noreply.github.com>
2020-05-20 20:27:24 -0500

    FIX Duplicate output layer

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 5 - Evaluating, Improving and Tuning the ANN/ann_k_fold__cross_validation.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 5 - Evaluating, Improving and Tuning the ANN/ann_k_fold__cross_validation.py
@@ -44,7 +44,6 @@ def build_classifier():
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer= 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
 
